912d9c7707f3797995622288d3285b89f742ae9a
sem laan <semlaan@dds.nl>
2020-08-09 20:46:56 +0200

    Added a dictionary that matches the tiletype enum to tile objects and added serialized fields for all the tile objects

--- Assets/Scripts/LevelGrid/TileGrid.cs
+++ Assets/Scripts/LevelGrid/TileGrid.cs
@@ -19,7 +19,35 @@ public enum TileType
 
 public class TileGrid : MonoBehaviour
 {
-    
+
+    [SerializeField] private TileType[,] tilegrid = new TileType[15, 15];
+    [SerializeField] private TileType tileBrush = TileType.empty;
+
+    [SerializeField] private Vector2Int levelSize;
+
+    [Header("Tile objects")]
+    [SerializeField] private GameObject empty = null;
+    [SerializeField] private GameObject wall = null,
+                                        conveyorLeft = null, conveyorRight = null, conveyorUp = null, conveyorDown = null,
+                                        conveyorTurnLeft = null, conveyorTurnRight = null;
+
+    private Dictionary<TileType, GameObject> tileDict = null;
     
 
+
+    private void Awake()
+    {
+
+        tileDict = new Dictionary<TileType, GameObject>()
+        {
+            {TileType.empty, empty},
+            {TileType.wall, wall},
+            {TileType.conveyorLeft, conveyorLeft},
+            {TileType.conveyorRight, conveyorRight},
+            {TileType.conveyorUp, conveyorUp},
+            {TileType.conveyorDown, conveyorDown},
+            {TileType.conveyorTurnLeft, conveyorTurnLeft},
+            {TileType.conveyorTurnRight, conveyorTurnRight},
+        };
+    }
 }
