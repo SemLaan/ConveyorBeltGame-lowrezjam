15c0aa828f1950a651dcc72e3f5027d4dbbf8df0
sem laan <semlaan@dds.nl>
2020-08-09 12:39:04 +0200

    Added a tile grid script with an enum for all the tile types

---
+++ Assets/Scripts/LevelGrid/TileGrid.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public enum TileType
+{
+
+    empty,
+    wall,
+    conveyorLeft,
+    conveyorRight,
+    conveyorUp,
+    conveyorDown,
+    conveyorTurnLeft,
+    conveyorTurnRight,
+}
+
+
+public class TileGrid : MonoBehaviour
+{
+    
+    
+
+}
