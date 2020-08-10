68f164c98eac1b54cc91271f2b201e5f6df13212
sem laan <semlaan@dds.nl>
2020-08-10 21:12:18 +0200

    Made a base tile abstract class

---
+++ Assets/Scripts/LevelGrid/Tiles/BaseTile.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class BaseTile : MonoBehaviour
+{
+    
+    public void TileAction(int fixedUpdateCount, Transform player) 
+    {
+        
+    }
+}
