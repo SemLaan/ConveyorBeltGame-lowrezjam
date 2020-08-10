5fa5fcb20c1aaa4f358f1ae5df9a7617c3868424
T.S.Omta <timo.omta@hotmail.com>
2020-08-10 17:21:47 +0200

    Made a new scene, added a player and added a movement script for the player

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField] public Vector3 direction;
+
+    void Update()
+    {
+        transform.position += direction * Time.deltaTime;
+    }
+}
