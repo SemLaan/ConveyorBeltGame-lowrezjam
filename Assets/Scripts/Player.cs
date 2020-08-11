6c8320403895b5eb5821bdc301db38698de13a30
T.S.Omta <timo.omta@hotmail.com>
2020-08-11 12:09:27 +0200

    The character stops when the player presses space

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,9 +5,14 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     [SerializeField] public Vector3 direction;
+    [SerializeField] public int speed;
 
     void Update()
     {
-        transform.position += direction * Time.deltaTime;
+        transform.position += direction * speed * Time.deltaTime;
+        if (Input.GetKey(KeyCode.Space))
+        {
+            speed = 0;
+        }
     }
 }
